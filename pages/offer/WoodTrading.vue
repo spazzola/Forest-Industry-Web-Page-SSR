4aafa14f731bcd48f941e43c9e6a74ed88bb7ead
pikseo01 <100206706+pikseo01@users.noreply.github.com>
2022-08-16 10:29:17 +0200

    Add files via upload

--- pages/offer/WoodTrading.vue
+++ pages/offer/WoodTrading.vue
@@ -1,78 +1,113 @@
 <template>
   <div>
     <nav-menu></nav-menu>
-    <div>
-      <h1 class="my-header">
-        Handel drewnem tartacznym – świerk, modrzew, cedr syberyjski i inne
-      </h1>
-      <div class="content pcontent">
-        <p style="text-align: justify">
-          W naszej ofercie można znaleźć zarówno drzewo konstrukcyjne, tarasowe,
-          jak i elewacyjne. Do sprzedania mamy deski tarasowe głównie z takiego
-          drewna jak: sosna, modrzew, cedr oraz świerk syberyjski — nasz tartak
-          proponuje je w mniejszych wymiarach od 2,5 m, przez średnie (np. 4 m
-          lub 6 m) aż do 14 m. Dodatek do oferty stanowią natomiast takie
-          gatunki jak: olcha, jesion, dąb czy buk. Nasze okrągłe drewno iglaste
-          oraz liściaste oferujemy przede wszystkim firmom — jest to bowiem
-          idealne drewno konstrukcyjne i budowlane do wznoszenia budynków czy
-          wykonywania remontów. Nic jednak nie stoi na przeszkodzie, żeby drzewo
-          tartaczne kupiły także osoby prywatne. To doskonały wybór na tarasy
-          czy elewację.
-        </p>
+    <div class="page_offer">
+     <header class="page_header wood">
 
-        <strong
-          >Posiadamy w ofercie cenne gatunki drewna na sprzedaż, w tym:</strong
-        >
-        <ul style="margin-left: 40px">
-          <li>świerk syberyjski,</li>
-          <li>modrzew syberyjski,</li>
-          <li>cedr syberyjski,</li>
-          <li>sosna.</li>
-        </ul>
+        <h1 class="page_heading flex" style="align-items: center;">Handel drewnem tartacznym – świerk, modrzew, cedr syberyjski i inne
+</h1>
 
-        <strong>Okazjonalnie można u nas zakupić też takie drewno jak:</strong>
+     </header>
 
-        <ul style="margin-left: 40px">
-          <li>dąb</li>
-          <li>buk,</li>
-          <li>jesion,</li>
-          <li>jawor,</li>
-          <li>olcha.</li>
-        </ul>
 
-        <p style="text-align: justify">
-          Każda nasza deska tarasowa bądź elewacyjna — czy to modrzewiowa,
-          dębowa, jesionowa czy bukowa — jest teraz dostępna za niewielkie
-          pieniądze. Cena jest ustalana zawsze indywidualnie na podstawie ilości
-          zamówionego asortymentu. Stali klienci mają gwarancję, że materiały
-          budowlane dotrą do nich przez cały rok. Dodatkowo mogą oni zamówić
-          drewno w dowolnej ilości.
-        </p>
-        <h2>Skąd pochodzi nasze drewno?</h2>
-        <p style="text-align: justify">
-          Handel drewnem tartacznym rozpoczęliśmy już lata temu — wówczas
-          sprzedawaliśmy nasze materiały lokalnie. Dzisiaj jesteśmy w stanie
-          pochwalić się znacznym rozwojem, gdyż ludzie w całej Europie
-          wykorzystują drewno na elewację i do remontów. Aby usprawnić sprzedaż
-          drewna tartacznego, uruchomiliśmy kilka składnic za granicami kraju —
-          sprowadzamy m.in. drewno tartaczne z Czech, Kazachstanu oraz Słowacji.
-          Ceny drewna w Czechach są znacznie niższe niż w innych krajach,
-          dlatego też handel jest bardziej opłacalny. A dodatkowo sprowadzanie
-          drewna z Kazachstanu czy Słowacji pozwala zachować ciągły zapas
-          materiałów, bez względu na wielkość zamówienia.
-        </p>
-        <p style="text-align: justify">
-          Jeśli więc planują Państwo wykonać elewację bądź taras drewniany z
-          modrzewia syberyjskiego, świerku, sosny czy innego cennego gatunku
-          drewna, zachęcamy do kontaktu. Dysponujemy takimi materiałami jak
-          kantówka, tarcica czy deska gładka — z nami mogą Państwo kupić drewno
-          z Czech, Kazachstanu, ze Śląska i Słowacji oraz wykorzystać je do
-          różnych celów, np. przeznaczyć na elegancką i trwałą elewację.
-        </p>
+<div class="container-fluid offer">
+   <div class="width_container">
+  <div class="row">
+    <div class="col-lg-4 col-md-6">
+      <img class="section_image" src="../../static/wood1.jpg" alt="handel drewnem">
+    </div>
+    <div class="col-lg-8 col-md-6 flex">
+      <p>W naszej ofercie można znaleźć zarówno drzewo konstrukcyjne, tarasowe, jak i elewacyjne. Do sprzedania mamy deski tarasowe głównie z takiego drewna jak: sosna, modrzew, cedr oraz świerk syberyjski — nasz tartak proponuje je w mniejszych wymiarach od 2,5 m przez średnie (np. 4 m lub 6 m) aż do 14 m. Dodatek do oferty stanowią natomiast takie gatunki jak: olcha, jesion, dąb czy buk. Nasze okrągłe drewno iglaste oraz liściaste oferujemy przede wszystkim firmom — jest to bowiem idealne drewno konstrukcyjne i budowlane do wznoszenia budynków czy wykonywania remontów. Nic jednak nie stoi na przeszkodzie, żeby drzewo tartaczne kupiły także osoby prywatne. To doskonały wybór na tarasy czy elewację.</p>
+      <p><strong>Posiadamy w ofercie cenne gatunki drewna na sprzedaż, w tym:</strong></p>
+      <ul style="margin-left: 40px;">
+        <li>świerk syberyjski,</li>
+        <li>modrzew syberyjski,</li>
+        <li>cedr syberyjski,</li>
+        <li>sosnę.</li>
+      </ul>
+      <p><strong>Okazjonalnie można u nas zakupić też takie drewno jak:</strong></p>
+      <ul style="margin-left: 40px;">
 
-        Handlujemy drewnem wyłącznie posiadającym certyfikaty PEFC oraz
-        FSC&reg;.
-      </div>
+        <li>dąb</li>
+        <li>buk,</li>
+        <li>jesion,</li>
+        <li>jawor,</li>
+        <li>olcha.</li>
+      </ul>
+
+      <p>Każda nasza deska tarasowa bądź elewacyjna — czy to modrzewiowa, dębowa, jesionowa czy bukowa — jest teraz dostępna za niewielkie pieniądze. Cena jest ustalana zawsze indywidualnie na podstawie ilości zamówionego asortymentu. Stali klienci mają gwarancję, że materiały budowlane dotrą do nich przez cały rok. Dodatkowo mogą oni zamówić drewno w dowolnej ilości.</p>
+    </div>
+  </div>
+ </div>
+</div>
+
+
+<div class="container-fluid offer">
+  <div class="width_container">
+  <div class="row">
+
+       <div class="col-xl-8 col-lg-12 flex">
+        <h2 class="section_heading">Skąd pochodzi nasze drewno?</h2>
+      <p>Handel drewnem tartacznym rozpoczęliśmy już lata temu — wówczas sprzedawaliśmy nasze materiały lokalnie. Dzisiaj jesteśmy w stanie pochwalić się znacznym rozwojem, gdyż ludzie w całej Europie wykorzystują drewno na elewację i do remontów. Aby usprawnić sprzedaż drewna tartacznego, uruchomiliśmy kilka składnic za granicami kraju — sprowadzamy m.in. drewno tartaczne z Czech, Kazachstanu oraz Słowacji. Ceny drewna w Czechach są znacznie niższe niż w innych krajach, dlatego też handel jest bardziej opłacalny. A dodatkowo sprowadzanie drewna z Kazachstanu czy Słowacji pozwala zachować ciągły zapas materiałów, bez względu na wielkość zamówienia.</p>
+      <p>Jeśli więc planują Państwo wykonać elewację bądź taras drewniany z modrzewia syberyjskiego, świerku, sosny czy innego cennego gatunku drewna, zachęcamy do kontaktu. Dysponujemy takimi materiałami jak kantówka, tarcica czy deska gładka — z nami mogą Państwo kupić drewno z Czech, Kazachstanu, ze Słowacji, a także ze Śląska oraz wykorzystać je do różnych celów, np. przeznaczyć na elegancką i trwałą elewację.</p>
+    </div>
+
+
+    <div class="col-xl-4 col-lg-12  flex" >
+     <img class="section_image" src="../../static/wood2.jpg" alt="drewno tartaczne">
+    </div>
+ 
+  </div>
+ </div>
+</div>
+
+
+<div class="container-fluid offer gallery">
+   <div class="width_container">
+  <div class="row">
+
+       <div class="col-lg-4 col-md-6 flex">
+        <img class="section_image" src="../../static/wood3.jpg" alt="drewno z tartaku">
+    </div>
+
+
+    <div class="col-lg-4 col-md-6 flex"><img class="section_image" src="../../static/wood4.jpg" alt="handel drewnem z tartaku">
+    </div>
+
+    <div class="col-lg-4 col-md-6 flex"><img class="section_image" src="../../static/wood5.jpg" alt="drewno z tartaku handel">
+    </div>
+ 
+  </div>
+  </div>
+ 
+</div>
+
+<div class="container-fluid offer">
+  <div class="width_container">
+  <div class="row">
+
+       <div class="col-xl-8 col-lg-12 flex">
+        <h2 class="section_heading">Handel drewnem tartacznym z certyfikatami PEFC oraz FSC®</h2>
+      <p>Handel drewnem tartacznym podlega ścisłej kontroli przede wszystkim pod względem legalności jego nabycia. Wiele firm może się więc zastanawiać, jak kupić drewno, by było ono dobre jakościowo, niedrogie i legalne. Obecnie ceny drewna w Czechach należą do jednych z niższych, ale by mieć pewność zgodności surowca z przepisami, warto wybrać tartak, który posiada niezbędne certyfikaty.</p>
+      <p>Cedr, świerk, modrzew syberyjski czy dowolne inne drewno tartaczne oferowane przez tartak Forest Industry S.A. — także to sprowadzane z Czech lub Kazachstanu — posiada certyfikaty PEFC oraz FSC®. Oznacza to, że handlujemy drewnem zgodnie z obowiązującymi przepisami UE. Ponadto drewno jest według nich produkowane i przetwarzane.</p>
+  
+    </div>
+
+
+    <div class="col-xl-4 col-lg-12 flex">
+     <img class="section_image" src="../../static/7.jpg" alt="całoroczny dom z bali">
+    </div>
+ 
+  </div>
+ </div>
+</div>
+
+
+
+
+
+
+
     </div>
     <div class="row, myrow">
       <footer class="col the-footer">
@@ -164,5 +199,107 @@ i {
 
 .myrow {
   margin-left: 0 !important;
+}
+
+.page_offer {
+  padding-bottom: 200px;
+}
+.page_offer * {
+background-color: transparent !important;
+}
+
+.page_header {
+  background-image: url("../../static/loghome1.jpg");
+   background-position: bottom;
+  background-size: cover;
+  width: 100vw;
+  height: 650px;
+ 
+ 
+}
+
+.page_header.wood {
+   background-image: url("../../static/background.jpg");
+}
+
+.flex {
+  display: flex;
+  justify-content: center;
+
+  flex-direction: column;
+}
+
+.page_heading {
+  margin: 0; 
+  padding: 0;
+  background-color: rgba(255,255,255, .4) !important;
+  height: 100%;
+  width: 100%;
+  
+  color: white;
+  text-shadow: 3px 3px #000;
+}
+
+.page_offer p {
+  font-weight: 500;
+  text-align: justify;
+}
+
+.width_container {
+  max-width: 80%;
+  margin: 0 auto;
+  padding-left: 15px;
+}
+
+.container-fluid.offer {
+  margin-top: 80px;
+}
+
+.section_image {
+  width: 100%;
+}
+
+.section_heading {
+  align-self: flex-start;
+}
+
+.gallery {
+  background-color:rgb(136, 158, 129) !important;
+
+}
+
+.gallery img {
+  margin: 25px 0;
+}
+.background_image {
+   background-image: url("../../static/loghome1.jpg");
+   background-size: cover;
+   background-position: center;
+}
+
+.contact_button {
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+   background-color:rgb(136, 158, 129) !important;
+   width: 150px;
+   padding: 10px 0;
+   color: black;
+   text-transform: uppercase;
+   font-weight: bold;
+   transition: .3s;
+   font-size: 16px;
+}
+.contact_button:hover {
+  color: white !important;
+  background-color:rgb(71 82 68) !important;
+}
+
+@media(max-width: 1150px) {
+  .width_container {
+    max-width: 95%;
+    margin: 0 auto;
+
+  }
 }
 </style>
